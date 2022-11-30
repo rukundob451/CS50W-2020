4285d1bc7aee591ded2c60ead72b5c23eeab01d2
Ira Krakow <ira.krakow@gmail.com>
2022-11-30 18:28:35 -0500

    tweak settings.py to get hello message

--- 3_Django/lecture3/hello/views.py
+++ 3_Django/lecture3/hello/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def brian(request):
+    return HttpResponse("Hello, Brian")
--- 3_Django/lecture3/lecture3/settings.py
+++ 3_Django/lecture3/lecture3/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+^6q#y#c&8#*7fmk3!ga+$(^e!=nv$636#)etpq&e!z2xj7*h)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
