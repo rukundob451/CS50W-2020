f528036560a4fea3a23aa8a786ec45385256672f
Ira Krakow <ira.krakow@gmail.com>
2022-12-02 07:25:24 -0500

    change show_keys.py

--- 2_Python/My_Examples/2_Sequences_And_File_Operations/Case_Study_1/2_Show_keys.py
+++ 2_Python/My_Examples/2_Sequences_And_File_Operations/Case_Study_1/2_Show_keys.py
@@ -1,5 +1,5 @@
 d = {"john": 40, "peter": 45}  # d is a dictionary
 print(list(d.keys()))
 print(list(d.values()))
-# ['john, 'peter']  Prints the keys of the dictionary
-# [40,45] prints the values in the dictionary
+# ['john, 'peter']  Prints the keys of the dictionary as a list
+# [40,45] prints the values in the dictionary as a list
