74511b2dc644341505d08fe5274ba34d01b36b0a
Ira Krakow <ira.krakow@gmail.com>
2022-12-02 15:43:38 -0500

    problem 5 print string with even indices

--- 2_Python/My_Examples/2_Sequences_And_File_Operations/Case_Study_1/5_Print_Strings_With_Even_Indexes.py
+++ 2_Python/My_Examples/2_Sequences_And_File_Operations/Case_Study_1/5_Print_Strings_With_Even_Indexes.py
@@ -8,7 +8,7 @@ aString = input("Enter a string: ")
 my_list = []
 
 for x in range(len(aString)):
-    # modulus 2 function test
+    # modulus 2 function tests whether the index is even
     if x % 2 == 0:
         my_list.append(aString[x])
 print(listToString(my_list))
