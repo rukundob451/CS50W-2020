70e409f47e219e4cb59f16c98614905ad88d1fa0
Ira Krakow <ira.krakow@gmail.com>
2022-12-02 15:41:01 -0500

    Number 5

--- 2_Python/My_Examples/2_Sequences_And_File_Operations/Case_Study_1/5_Print_Strings_With_Even_Indexes.py
+++ 2_Python/My_Examples/2_Sequences_And_File_Operations/Case_Study_1/5_Print_Strings_With_Even_Indexes.py
@@ -1,14 +1,14 @@
+def listToString(s):
+    str1 = ""
+    return (str1.join(s))
+
+
 aString = input("Enter a string: ")
 
 my_list = []
-for ch in aString:
-    if 
 
-
-for ch in range(len(aString))
-
-
-for x in aString:
-    # modulus 2 function test 
+for x in range(len(aString)):
+    # modulus 2 function test
     if x % 2 == 0:
-        print(aString[x])
+        my_list.append(aString[x])
+print(listToString(my_list))
