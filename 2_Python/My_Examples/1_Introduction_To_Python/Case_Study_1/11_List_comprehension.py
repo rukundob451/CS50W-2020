7d6276eeca7f902c043c0da10340e1d0667918a7
Ira Krakow <ira.krakow@gmail.com>
2022-12-09 12:45:32 -0500

    11_Remove_elements _in_List_Print_result

---
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/11_List_comprehension.py
@@ -0,0 +1,7 @@
+my_list = [12, 24, 35, 70, 88, 120, 155]
+
+# remove elements at 0th, 4th, and 5th index
+result = [value for i, value in enumerate(my_list) if i not in (0, 4, 5)]
+
+# print the result
+print(result)
