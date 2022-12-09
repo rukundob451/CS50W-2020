e3805a14e968e0506e821a9059786ea911870f99
Ira Krakow <ira.krakow@gmail.com>
2022-12-09 13:30:05 -0500

    13_List_Of_5_Random_Numbers_Divisible_By_5_and_7_Between_1_and_1000_inclusive.py

---
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/13_List_Of_5_Random_Numbers_Divisible_By_5_and_7_Between_1_and_1000_inclusive.py
@@ -0,0 +1,6 @@
+import random
+my_list = [random.randint(1, 1000) for i in range(1001)]
+divisible_by_5_and_7 = [x for x in my_list if x % 5 == 0 and x % 7 == 0]
+
+
+print(divisible_by_5_and_7[0:5])
