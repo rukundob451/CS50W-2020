a7691e66581b1b14d71f84e64fce844efd44f7f4
Ira Krakow <ira.krakow@gmail.com>
2022-12-10 14:17:43 -0500

    Helloworld_backwards

---
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/5_Helloworld_backwards.py
@@ -0,0 +1,6 @@
+
+string = "H1e2l3l4o5w6o7r8l9d"
+
+for i, char in enumerate(string):
+    if i % 2 == 0:
+        print(char, end="")
--- 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/5_Palindrome_Test.py
+++
@@ -1,9 +0,0 @@
-def palindrome_test(sentence):
-    return sentence[::-1] == sentence
-
-
-sent = input("Sentence: ")
-if palindrome_test(sent.lower()):
-    print(f"{sent} is a palindrome!")
-else:
-    print(f"{sent} is not a palindrome!")
