5dc28dcefddd4ab660509906a7701fe9ad452d06
Ira Krakow <ira.krakow@gmail.com>
2022-12-10 14:13:51 -0500

    Problem 4

---
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/4_Print_Elements_In_List.py
@@ -0,0 +1,13 @@
+# print elements in a list
+a = [4, 7, 3, 2, 5, 9]
+
+for i, element in enumerate(a):
+  print(f"The element at index {i} is {element}.")
+
+#Output:
+#The element at index 0 is 4.
+#The element at index 1 is 7.
+#The element at index 2 is 3.
+#The element at index 3 is 2.
+#The element at index 4 is 5.
+#The element at index 5 is 9.
