f6b83e9ceed6256f6b8b161d4db7212de08b8cdb
Ira Krakow <ira.krakow@gmail.com>
2022-12-10 14:46:51 -0500

    6 Reverse A String

---
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/6_Reverse_A_String.py
@@ -0,0 +1,8 @@
+# Get the input string from the user
+string = input("Enter a string: ")
+
+# Reverse the string. Use negative step to do the reversal
+reversed_string = string[::-1]
+
+# Print the reversed string
+print(reversed_string)
