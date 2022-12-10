b22f436e605fd368084268dcb483170b47fe741e
Ira Krakow <ira.krakow@gmail.com>
2022-12-10 14:23:39 -0500

    Problem 8

--- 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/8_Intersection_Of_Two_Lists.py
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/8_Intersection_Of_Two_Lists.py
@@ -4,4 +4,4 @@ def intersect_Two_Lists(l1, l2):
 
 list1 = [1, 3, 6, 78, 35, 55]
 list2 = [12, 24, 35, 24, 88, 120, 155]
-print(intersect_Two_Lists(list1, list2))
+print(intersect_Two_Lists(list1, list2))  # [35]
