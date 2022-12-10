e87ca1a3d1a86c23fc55be40d4e341073dad57ee
Ira Krakow <ira.krakow@gmail.com>
2022-12-10 13:41:34 -0500

    problems 2 and 14

--- 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/14_Compute_Sum_of_fractions.py
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/14_Compute_Sum_of_fractions.py
@@ -2,9 +2,6 @@ def compute_sum(n):
     return sum([i / (i + 1) for i in range(1, n + 1)])
 
 
-n = int(input(("n: ")))  # input returns a string...must convert to int
+num = int(input(("n: ")))  # input returns a string...must convert to int
 # compute_sum returns float...need to convert to int to get answer
-print(compute_sum(n))  # format to 3 decimal places
-# some other examples
-print(compute_sum(55))
-print(compute_sum(533))
+print(compute_sum(num))  # format to 3 decimal places
---
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/2_List_Dicrionary_Keys.py
@@ -0,0 +1,3 @@
+# List dictionary keys with keys() function
+d = {"john": 40, "peter": 45}
+print(list(d.keys()))
