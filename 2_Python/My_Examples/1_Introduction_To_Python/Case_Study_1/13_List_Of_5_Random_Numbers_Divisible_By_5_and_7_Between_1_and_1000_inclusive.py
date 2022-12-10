f132fd8a2ae0fac36bc7406db47708f7397cf9c0
Ira Krakow <ira.krakow@gmail.com>
2022-12-10 14:29:12 -0500

    Probllems 12 and 13

--- 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/13_List_Of_5_Random_Numbers_Divisible_By_5_and_7_Between_1_and_1000_inclusive.py
+++ 2_Python/My_Examples/1_Introduction_To_Python/Case_Study_1/13_List_Of_5_Random_Numbers_Divisible_By_5_and_7_Between_1_and_1000_inclusive.py
@@ -2,5 +2,5 @@ import random
 my_list = [random.randint(1, 1000) for i in range(1001)]
 divisible_by_5_and_7 = [x for x in my_list if x % 5 == 0 and x % 7 == 0]
 
-
+# print the first 5 elements slicing the list
 print(divisible_by_5_and_7[0:5])
