24c4c2c761fdd7e0b76f13d1800f397f2dcc81c7
Ira Krakow <ira.krakow@gmail.com>
2022-12-11 20:03:39 -0500

    Encryption and decryption

--- 3_Django_Lecture/lecture3/lecture3/urls.py
+++ 3_Django_Lecture/lecture3/lecture3/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
 urlpatterns = [
+    path('','views.index'),
     path('admin/', admin.site.urls),
     path('hello', include('hello.urls'))
 ]
